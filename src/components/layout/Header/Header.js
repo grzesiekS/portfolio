01bb5a135659a0ea0500ce95e1a833f03339d5b7
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-10-01 19:05:32 +0200

    Add splash component

---
+++ src/components/layout/Header/Header.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Splash from '../Splash/Splash';
+
+const Header = () => (
+  <Splash />
+);
+
+export default Header;
---
+++ src/components/layout/Splash/Splash.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Splash.module.scss';
+
+const Splash = () => (
+  <header className={styles.root}>
+    <div className={styles.container}>
+      <h1>Front-end Developer</h1>
+    </div>
+  </header>
+);
+
+export default Splash;
