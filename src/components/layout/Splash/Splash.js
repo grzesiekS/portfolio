02e6086181c40bfdd57d90e1447e07ac174bc643
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-10-01 19:13:24 +0200

    Add new fonts plus add title in splash component

--- src/components/layout/Splash/Splash.js
+++ src/components/layout/Splash/Splash.js
@@ -4,7 +4,8 @@ import styles from './Splash.module.scss';
 const Splash = () => (
   <header className={styles.root}>
     <div className={styles.container}>
-      <h1>Front-end Developer</h1>
+      <h1 className={styles.title}>Front-end Developer</h1>
+      <h2>I code websites from scratches</h2>
     </div>
   </header>
 );
