8fe38d1b5c3dcc1a2787a32328da7c24b11d196b
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-10-01 19:39:58 +0200

    Add icon to Splash section

--- src/components/layout/Splash/Splash.js
+++ src/components/layout/Splash/Splash.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import styles from './Splash.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLaptopCode } from '@fortawesome/free-solid-svg-icons';
 
 const Splash = () => (
   <header className={styles.root}>
     <div className={styles.container}>
       <h1 className={styles.title}>Front-end Developer</h1>
+      <FontAwesomeIcon className={styles.icon}  icon={faLaptopCode} />
       <h2>I code websites from scratches</h2>
     </div>
   </header>
