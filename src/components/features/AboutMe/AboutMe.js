6bfd1641d184ff358c02109d2dd6cc861128d4ec
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-10-06 20:39:50 +0200

    Add content and styles for AboutMe component

--- src/components/features/AboutMe/AboutMe.js
+++ src/components/features/AboutMe/AboutMe.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import styles from './AboutMe.module.scss';
 
 const AboutMe = () => (
-  <div>
-    <h1>Lorem Ipsum</h1>
+  <div className={styles.container}>
+    <div className={styles.aboutMe}>
+      <h1 className={styles.title}>About Me</h1>
+    </div>
   </div>
 );
 
