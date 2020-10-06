9e848089c96a92a2b5829265a8b38b05b5d22740
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-10-06 20:51:30 +0200

    Add content section and styles for AboutMe component

--- src/components/features/AboutMe/AboutMe.js
+++ src/components/features/AboutMe/AboutMe.js
@@ -5,6 +5,7 @@ const AboutMe = () => (
   <div className={styles.container}>
     <div className={styles.aboutMe}>
       <h1 className={styles.title}>About Me</h1>
+      <p className={styles.content}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur consequat sodales sapien, id gravida est interdum sit amet. Nulla tempor ut magna sollicitudin interdum. Praesent in tincidunt nisl. Aenean molestie, nisl ut iaculis faucibus, risus magna dictum velit, vel imperdiet dolor ligula a diam. Sed et varius diam, a auctor magna. Suspendisse tempus posuere urna id tempor. Duis molestie nec urna eget ultricies.</p>
     </div>
   </div>
 );
