6a43d14e7d909b36a6eb3d414c25ea95dd7004df
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-10-17 23:56:46 +0200

    Create Techs component and implement it in SkillTechList component

--- src/components/features/SkillTechList/SkillTechList.js
+++ src/components/features/SkillTechList/SkillTechList.js
@@ -3,18 +3,27 @@ import PropTypes from 'prop-types';
 
 import styles from './SkillTechList.module.scss';
 import Skill from '../Skill/Skill';
+import Techs from '../Techs/Techs';
 
 const SkillTechList = (props) => (
-  <div className={styles.container}>
+  <div id='techSkills' className={styles.container}>
     <h1 className={styles.title}>Skills and Technologies</h1>
-    {props.skills.map(skill => (
-      <Skill key={props.skills.indexOf(skill)} skillName={skill} />
-    ))}
+    <div className={styles.skills}>
+      {props.skills.map(skill => (
+        <Skill key={props.skills.indexOf(skill)} skillName={skill} />
+      ))}
+    </div>
+    <div>
+      {props.techs.map(tech => (
+        <Techs key={props.techs.indexOf(tech)} techName={tech.techName}/>
+      ))}
+    </div>
   </div>
 );
 
 SkillTechList.propTypes = {
   skills: PropTypes.array,
+  techs: PropTypes.array,
 };
 
 export default SkillTechList;
