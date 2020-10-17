de04fb369ff9acab701e77a07dd168862755e25d
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-10-17 22:55:32 +0200

    Add Skill component and implent it in SkillTechList component

--- src/components/features/SkillTechList/SkillTechList.js
+++ src/components/features/SkillTechList/SkillTechList.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import styles from './SkillTechList.module.scss';
+import Skill from '../Skill/Skill';
 
 const SkillTechList = (props) => (
   <div className={styles.container}>
     <h1 className={styles.title}>Skills and Technologies</h1>
+    {props.skills.map(skill => (
+      <Skill key={props.skills.indexOf(skill)} skillName={skill} />
+    ))}
   </div>
 );
+
+SkillTechList.propTypes = {
+  skills: PropTypes.array,
+};
 
 export default SkillTechList;
