612582068c75c2997db47f6e9a0d622087bff1c8
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-10-18 11:38:30 +0200

    Add style for SkillTechList component

--- src/components/features/SkillTechList/SkillTechList.js
+++ src/components/features/SkillTechList/SkillTechList.js
@@ -8,15 +8,17 @@ import Techs from '../Techs/Techs';
 const SkillTechList = (props) => (
   <div id='techSkills' className={styles.container}>
     <h1 className={styles.title}>Skills and Technologies</h1>
-    <div className={styles.skills}>
-      {props.skills.map(skill => (
-        <Skill key={props.skills.indexOf(skill)} skillName={skill} />
-      ))}
-    </div>
-    <div>
-      {props.techs.map(tech => (
-        <Techs key={props.techs.indexOf(tech)} techName={tech.techName}/>
-      ))}
+    <div className={styles.flexBox}>
+      <div className={styles.skills}>
+        {props.skills.map(skill => (
+          <Skill key={props.skills.indexOf(skill)} skillName={skill} />
+        ))}
+      </div>
+      <div className={styles.techs}>
+        {props.techs.map(tech => (
+          <Techs key={props.techs.indexOf(tech)} techName={tech.techName}/>
+        ))}
+      </div>
     </div>
   </div>
 );
