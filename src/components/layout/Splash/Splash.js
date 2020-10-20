273c3cb31f665ae7cd9d4bcb452149219d5a0156
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-10-20 20:36:01 +0200

    Add media queries to Splash component

--- src/components/layout/Splash/Splash.js
+++ src/components/layout/Splash/Splash.js
@@ -5,7 +5,7 @@ import { faLaptopCode } from '@fortawesome/free-solid-svg-icons';
 
 const Splash = () => (
   <div className={styles.container}>
-    <h1 className={styles.title}>Front-end Developer</h1>
+    <h1 className={styles.title}>Junior Web Developer</h1>
     <FontAwesomeIcon className={styles.icon}  icon={faLaptopCode} />
     <h2 className={styles.subtitle}>I code websites from scratch</h2>
   </div>
