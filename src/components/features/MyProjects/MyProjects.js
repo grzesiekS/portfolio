6df73048974f97d315e519ea039a9928fb045b8c
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-10-21 20:09:00 +0200

    Add styles and structure for MyProjects component

--- src/components/features/MyProjects/MyProjects.js
+++ src/components/features/MyProjects/MyProjects.js
@@ -1,16 +1,25 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 import styles from './MyProjects.module.scss';
 
 class MyProject extends React.Component {
   render() {
+
+    const {title} = this.props;
+
     return(
       <div className={styles.container}>
-        <h2>MyProjects section</h2>
+        <div id='myProjects' className={styles.myProjects}>
+          <h1 className={styles.title}>{title}</h1>
+        </div>
       </div>
     );
   }
 }
+
+MyProject.propTypes = {
+  title: PropTypes.string,
+};
 
 export default MyProject;
