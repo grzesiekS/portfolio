dd8a84a185f6a5fd2f722520becb5278a591e003
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-10-22 19:01:45 +0200

    Implement Project component in MyProjects

--- src/components/features/MyProjects/MyProjects.js
+++ src/components/features/MyProjects/MyProjects.js
@@ -2,17 +2,23 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import styles from './MyProjects.module.scss';
+import Project from '../Project/Project';
 
 class MyProject extends React.Component {
   render() {
 
-    const {title, description} = this.props;
+    const {title, description, projects} = this.props;
 
     return(
       <div className={styles.container}>
         <div id='myProjects' className={styles.myProjects}>
           <h1 className={styles.title}>{title}</h1>
           <p>{description}</p>
+          <div className={styles.flexBox}>
+            {projects.map(project => (
+              <Project key={project._id} {...project} />
+            ))}
+          </div>
         </div>
       </div>
     );
@@ -22,6 +28,7 @@ class MyProject extends React.Component {
 MyProject.propTypes = {
   title: PropTypes.string,
   description: PropTypes.string,
+  projects: PropTypes.array,
 };
 
 export default MyProject;
