58fc7ee3388e61ad80e6a63ef625d5eb691322af
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-11-29 14:17:59 +0100

    Add chenging project page method

--- src/components/features/MyProjects/MyProjects.js
+++ src/components/features/MyProjects/MyProjects.js
@@ -1,13 +1,41 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import clsx from 'clsx';
 
 import styles from './MyProjects.module.scss';
 import Project from '../Project/Project';
 
 class MyProject extends React.Component {
+  state = {
+    activePage: 0,
+    projectsOnPage: 3,
+  }
+
+  changeActivePage = (pageNumber) => {
+    this.setState({
+      ...this.state,
+      activePage: pageNumber,
+    });
+  }
+
   render() {
 
     const {title, description, projects, globalLanguage} = this.props;
+
+    const pagesCount = Math.ceil(projects.length / this.state.projectsOnPage);
+    const numbers = [];
+
+    for(let i = 0; i < pagesCount; i++) {
+      numbers.push(
+        <p
+          key={i}
+          className={this.state.activePage === i ? clsx(styles.pageNumber, styles.active) : styles.pageNumber}
+          onClick={() => this.changeActivePage(i)}
+        >
+          {i + 1}
+        </p>
+      );
+    }
 
     return(
       <div className={styles.container}>
@@ -15,8 +43,15 @@ class MyProject extends React.Component {
           <h1 className={styles.title}>{title}</h1>
           <p>{description}</p>
           <div className={styles.flexBox}>
-            {projects.map(project => (
-              <Project key={project._id} globalLanguage={globalLanguage} {...project} />
+            {projects
+              .slice(this.state.activePage * this.state.projectsOnPage, (this.state.activePage + 1) * this.state.projectsOnPage)
+              .map(project => (
+                <Project key={project._id} globalLanguage={globalLanguage} {...project} />
+              ))}
+          </div>
+          <div className={styles.pageNav}>
+            {numbers.map(number => (
+              number
             ))}
           </div>
         </div>
