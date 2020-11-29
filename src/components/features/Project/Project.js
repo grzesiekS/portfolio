45422a2aa3f005c410ee8a6fcf1b16aaf0c34381
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-11-29 12:31:09 +0100

    Add animations and styles for project component

--- src/components/features/Project/Project.js
+++ src/components/features/Project/Project.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import clsx from 'clsx';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAngleDown, faGlobeEurope } from '@fortawesome/free-solid-svg-icons';
+import { faAngleUp, faGlobeEurope } from '@fortawesome/free-solid-svg-icons';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 import styles from './Project.module.scss';
@@ -11,14 +12,20 @@ import Button from '../../common/Button/Button';
 
 class Project extends React.Component {
   state = {
-    descSecVis: false,
-    descSecStyles: [styles.description],
+    descriptionActive: false,
   }
 
   fliterDescriptionData = () => (
     this.props.descriptionData
       .filter(data => data.language === this.props.globalLanguage)[0].description
   );
+
+  changeDescriptionActive = () => {
+    this.setState({
+      ...this.state,
+      descriptionActive: !this.state.descriptionActive,
+    });
+  }
 
   render() {
 
@@ -30,10 +37,17 @@ class Project extends React.Component {
         <img className={styles.image} src={picture} alt={title} />
 
         <div className={styles.rotate}>
-          <FontAwesomeIcon icon={faAngleDown} className={styles.rotateIcon} onClick={() => this.descSecClassChange()} />
+          <FontAwesomeIcon
+            icon={faAngleUp}
+            className={this.state.descriptionActive
+              ? clsx(styles.rotateIcon, styles.active) : styles.rotateIcon}
+            onClick={() => this.changeDescriptionActive()}
+          />
         </div>
 
-        <section className={this.state.descSecStyles.join(' ')}>
+        <section
+          className={this.state.descriptionActive
+            ? clsx(styles.description, styles.active) : styles.description}>
           <p>{this.fliterDescriptionData()}</p>
           <div className={styles.buttons}>
             <Button href={link} target="_blank">
