445df9356240493905957e3c042ada58aa35b31f
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-11-29 14:50:50 +0100

    Add icon button type

--- src/components/common/Button/Button.js
+++ src/components/common/Button/Button.js
@@ -2,12 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Button.module.scss';
 
-const Button =({ children, Type = 'a', animation = false, ...props}) => {
+const Button =({ children, Type = 'a', animation = false, subType, ...props}) => {
   const classes = [];
 
   classes.push(styles.main);
 
   if(animation) classes.push(styles.fadeIn);
+
+  switch(subType) {
+    case 'icon':
+      classes.push(styles.icon);
+      break;
+    default:
+      classes.push(null);
+  }
 
   return (
     <Type className={classes.join(' ')} {...props}>
@@ -20,6 +28,7 @@ Button.propTypes = {
   children: PropTypes.node,
   Type: PropTypes.string,
   animation: PropTypes.bool,
+  subType: PropTypes.string,
 };
 
 export default Button;
