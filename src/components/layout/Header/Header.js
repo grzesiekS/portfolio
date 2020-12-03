78db99e25a79105514dd128be1cacc8fe7d5ca67
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-12-03 18:12:17 +0100

    Remove Splash from Header component and implement it in Homepage view

--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import Splash from '../Splash/Splash';
 import TopBar from '../TopBar/TopBarContainer';
 import styles from './Header.module.scss';
 
 const Header = () => (
   <header className={styles.root}>
     <TopBar />
-    <Splash />
   </header>
 );
 
--- src/components/views/Homepage.js
+++ src/components/views/Homepage.js
@@ -2,9 +2,11 @@ import React from 'react';
 import AboutMe from '../features/AboutMe/AboutMeContainer';
 import SkillTechList from '../features/SkillTechList/SkillTechListContainer';
 import MyProjects from '../features/MyProjects/MyProjectsContainer';
+import Splash from '../layout/Splash/Splash';
 
 const Homepage = () => (
   <div>
+    <Splash />
     <AboutMe />
     <SkillTechList />
     <MyProjects />
