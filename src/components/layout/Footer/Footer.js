77153713b02591db11579bfe3426e804a16c1966
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-12-12 16:23:32 +0100

    Create Footer and implement it in MainLayout

---
+++ src/components/layout/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import styles from './Footer.module.scss';
+
+const Footer = () => (
+  <footer className={styles.footer}>
+    <p>Find Me on social media</p>
+    <ul>
+      <li>Linked In</li>
+    </ul>
+  </footer>
+);
+
+export default Footer;
