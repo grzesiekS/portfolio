8514535d599e3c9993d0f49bc47b351cabc07c38
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-12-28 17:26:42 +0100

    Create component ErrorMsg

---
+++ src/components/features/ErrorMsg/ErrorMsg.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import styles from './ErrorMsg.module.scss';
+
+const ErrorMsg = ({title, msg}) => (
+  <div className={styles.container}>
+    <h1 className={styles.title}>{title}</h1>
+    <p className={styles.msg}>{msg}</p>
+  </div>
+);
+
+ErrorMsg.propTypes = {
+  title: PropTypes.string,
+  msg: PropTypes.string,
+};
+
+ErrorMsg.defaultProps = {
+  title: `Website You are looking for, doesn't exist.`,
+  msg: `The entered address is incorrect.`,
+};
+
+export default ErrorMsg;
