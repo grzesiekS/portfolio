e9b189a5254f9083f7a46f775aad3546310242cf
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-12-30 15:26:11 +0100

    Add controller and rout for sending emails

--- backend/controllers/forms.controller.js
+++ backend/controllers/forms.controller.js
@@ -1,10 +1,48 @@
 const Forms = require('../db/forms.json');
+const nodemailer = require('nodemailer');
+const sanitize = require('mongo-sanitize');
+
+const emailPass = require('../../secureVariables/emailPass');
 
 exports.loadAll = async (req, res) => {
   try {
     const result = Forms;
     if(!result) res.status(404).json({ forms: 'Not found' });
     else res.json(result);
+  } catch(err) {
+    res.status(500).json(err);
+  }
+};
+
+exports.sendEmail = async (req, res) => {
+  try {
+
+    const bodySanitize = sanitize(req.body);
+    const {name, lastName, email, phoneNo, message} = bodySanitize;
+    const htmlMessage = message.replace(/\n/g, '<br>\n') + '<br><br>' 
+    + `<b>${name} ${lastName}</b><br>`
+    + `<b>E-Mail: ${email}</b><br>`
+    + `<b>Phone number: ${phoneNo}</b>`;
+
+    let transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: emailPass.emailDetails.user,
+        pass: emailPass.emailDetails.pass,
+      },
+    });
+  
+    // send mail with defined transport object
+    await transporter.sendMail({
+      from: emailPass.emailDetails.user, // sender address
+      to: emailPass.emailDetails.myEmail, // list of receivers
+      subject: 'Hello ✔', // Subject line
+      html: htmlMessage, // html body
+    });
+
+    res.json({message: 'email sent'});
+
+    // transporter.close();
   } catch(err) {
     res.status(500).json(err);
   }
--- backend/routes/forms.routes.js
+++ backend/routes/forms.routes.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const forms = require('../controllers/forms.controller');
 
 router.get('/forms', forms.loadAll);
+router.post('/forms/send-email', forms.sendEmail);
 
 module.exports = router;
