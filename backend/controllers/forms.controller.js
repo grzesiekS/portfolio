131c64d23bdf6c23630ddb607a38c1748cb7e95d
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2021-01-05 16:50:20 +0100

    Add validation for contact form in server site

--- backend/controllers/forms.controller.js
+++ backend/controllers/forms.controller.js
@@ -19,28 +19,42 @@ exports.sendEmail = async (req, res) => {
 
     const bodySanitize = sanitize(req.body);
     const {name, lastName, email, phoneNo, subject, message} = bodySanitize;
-    const htmlMessage = message.replace(/\n/g, '<br>\n') + '<br><br>' 
-    + `<b>${name} ${lastName}</b><br>`
-    + `<b>E-Mail: ${email}</b><br>`
-    + `<b>Phone number: ${phoneNo}</b>`;
+    if (name
+      && lastName
+      && email
+      && email.split('@').length === 2
+      && email.split('.').length === 2
+      && email.split('.')[1] !== ''
+      && email.indexOf(' ') === -1
+      && phoneNo
+      && subject
+      && message
+    ) {
+      const htmlMessage = message.replace(/\n/g, '<br>\n') + '<br><br>' 
+      + `<b>${name} ${lastName}</b><br>`
+      + `<b>E-Mail: ${email}</b><br>`
+      + `<b>Phone number: ${phoneNo}</b>`;
 
-    let transporter = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-        user: emailPass.emailDetails.user,
-        pass: emailPass.emailDetails.pass,
-      },
-    });
-  
-    // send mail with defined transport object
-    await transporter.sendMail({
-      from: emailPass.emailDetails.user, // sender address
-      to: emailPass.emailDetails.myEmail, // list of receivers
-      subject: subject, // Subject line
-      html: htmlMessage, // html body
-    });
+      let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: emailPass.emailDetails.user,
+          pass: emailPass.emailDetails.pass,
+        },
+      });
+    
+      // send mail with defined transport object
+      await transporter.sendMail({
+        from: emailPass.emailDetails.user, // sender address
+        to: emailPass.emailDetails.myEmail, // list of receivers
+        subject: subject, // Subject line
+        html: htmlMessage, // html body
+      });
 
-    res.json({message: 'email sent'});
+      res.json({message: 'email sent'});
+    } else {
+      res.json({message: 'email not sent'});
+    }
 
     // transporter.close();
   } catch(err) {
