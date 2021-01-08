60b67733c4ab1f610a1cb6ffbc2c55856dedeaf5
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2021-01-08 17:32:12 +0100

    Add icon in Modal component

--- src/components/features/Modal/Modal.js
+++ src/components/features/Modal/Modal.js
@@ -2,14 +2,24 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faThumbsUp, faSurprise } from '@fortawesome/free-solid-svg-icons';
+
 import styles from './Modal.module.scss';
 
 const Modal = ({type, display, msg, children}) => {
+
+  const handleIconTypeSelection = type => {
+    if (type === 'success') return faThumbsUp;
+    if (type === 'error') return faSurprise;
+  };
+
   return (
     <div className={styles.container}>
       <div className={clsx(styles.message, styles[type])}>
+        <FontAwesomeIcon className={styles.icon} icon={handleIconTypeSelection(type)} />
         <h2 className={styles.subTitle}>
-          Test Message
+          {msg}
         </h2>
       </div>
       {children}
