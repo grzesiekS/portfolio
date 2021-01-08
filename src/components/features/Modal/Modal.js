19560249ff9f8166f4deb010b4b26465a8010589
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2021-01-08 16:38:41 +0100

    Modify structure of Modal component

--- src/components/features/Modal/Modal.js
+++ src/components/features/Modal/Modal.js
@@ -4,21 +4,23 @@ import clsx from 'clsx';
 
 import styles from './Modal.module.scss';
 
-const Modal = ({msg, type}) => {
+const Modal = ({msg, type, children}) => {
   return (
-    <div className={styles.container}>
+    <section>
       <div className={clsx(styles.message, styles[type])}>
         <h2 className={styles.subTitle}>
           {msg}
         </h2>
       </div>
-    </div>
+      {children}
+    </section>
   );
 };
 
 Modal.propTypes = {
   msg: PropTypes.string,
   type: PropTypes.string,
+  children: PropTypes.node,
 };
 
 export default Modal;
