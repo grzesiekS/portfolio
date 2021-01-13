2f722acd423dd6631f2041b347f83bade35ffa3c
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2021-01-13 19:01:34 +0100

    Create component SendMsg

---
+++ src/components/common/SendMsg/SendMsg.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from './SendMsg.module.scss';
+
+const SendMsg = () => (
+  <div className={styles.container}>
+
+  </div>
+);
+
+export default SendMsg;
