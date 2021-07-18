18368483b06283a4f2936b9f97792b3600700d1a
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2021-07-18 21:24:27 +0200

    Add boxshadow and neon light effect for incon in Splash component

--- src/components/layout/Splash/Splash.js
+++ src/components/layout/Splash/Splash.js
@@ -6,7 +6,9 @@ import { faLaptopCode } from '@fortawesome/free-solid-svg-icons';
 const Splash = () => (
   <div className={styles.container}>
     <h1 className={styles.title}>Junior Web Developer</h1>
-    <FontAwesomeIcon className={styles.icon}  icon={faLaptopCode} />
+    <div className={styles.iconContainer}>
+      <FontAwesomeIcon className={styles.icon}  icon={faLaptopCode} />
+    </div>
     <h2 className={styles.subtitle}>I code websites from scratch</h2>
   </div>
 );
